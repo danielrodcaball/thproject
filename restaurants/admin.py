32d2cbd56eff1dd7cddd491367b124af13ce31d3
daniel <danielrodcaball@gmail.com>
2021-11-02 08:52:52 -0400

    add: admin models management

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from restaurants.models import DietType, Restaurant, Table, Diner
+from restaurants.models.reservation import Reservation
 
-# Register your models here.
+
+class DietTypeAdmin(admin.ModelAdmin):
+    list_display = 'name',
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = 'datetime',
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location_lat', 'location_long', 'open_time', 'close_time')
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('capacity', 'restaurant')
+
+
+class DinerAdmin(admin.ModelAdmin):
+    list_display = 'name',
+
+
+admin.site.register(DietType, DietTypeAdmin)
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Table, TableAdmin)
+admin.site.register(Diner, DinerAdmin)
