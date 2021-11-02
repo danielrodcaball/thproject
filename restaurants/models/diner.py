01544ef5d12b2adb5e4c2a715b1ed9f635764e98
daniel <danielrodcaball@gmail.com>
2021-11-02 09:03:20 -0400

    fix: set diet types relation in restaurant and diner optional

--- restaurants/models/diner.py
+++ restaurants/models/diner.py
@@ -4,6 +4,6 @@ from restaurants.models.diettype import DietType
 
 class Diner(models.Model):
     name = models.CharField(max_length=255, verbose_name='Name')
-    diet_types = models.ManyToManyField(to=DietType)
+    diet_types = models.ManyToManyField(to=DietType, blank=True)
     house_location_lat = models.FloatField(verbose_name="Home Location Latitude")
     house_location_long = models.FloatField(verbose_name="Home Location Longitude")
--- restaurants/models/restaurant.py
+++ restaurants/models/restaurant.py
@@ -6,7 +6,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255, verbose_name='Name')
     open_time = models.TimeField()
     close_time = models.TimeField()
-    diet_type_endorsements = models.ManyToManyField(to=DietType)
+    diet_type_endorsements = models.ManyToManyField(to=DietType, blank=True)
     location_lat = models.FloatField(verbose_name='Location Latitude')
     location_long = models.FloatField(verbose_name='Location Longitude')
 
