d7ea6304f93c6bc557a8504a4076c0182663063d
daniel <danielrodcaball@gmail.com>
2021-11-02 08:37:29 -0400

    fix: add unique constraint to diet_type table

--- restaurants/models/diettype.py
+++ restaurants/models/diettype.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class DietType(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Diet Type')
+    name = models.CharField(max_length=50, verbose_name='Name')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_name')
+        ]
